ec3a5833c28551ad5d71ec4996ec97cde56cd399
gaughanln <gaughanln@yahoo.com>
2023-04-24 15:34:59 -0500

    moved about to home page

--- src/components/About.js
+++ src/components/About.js
@@ -5,17 +5,17 @@ import aboutPhoto from "./assets/gaughanln.png";
 
 function About() {
 return (
-<div class="container ">
-        <div class="row h-100">
-          <div class="col-md-6 my-auto text-center">
-            <img src={aboutPhoto} class="about-photo" alt="Lindsey Gaughan" />
+<div className ="container about-container">
+        <div className ="row h-100">
+          <div className ="col-md-6 my-auto text-center">
+            <img src={aboutPhoto} className ="about-photo" alt="Lindsey Gaughan" />
             
             {/* Background, photo, name, header, icons */}
           </div>
         
-          <div class="col-md-6 my-auto box rounded-pill d-flex justify-content-evenly">
+          <div className ="col-md-6 my-auto box rounded d-flex justify-content-evenly">
            
-          <p class = "about-me quote-wrapper text-center">I am an aspiring Full Stack Web Developer/Software Engineer with a background in Marketing, Project Management and Graphic Design. I have a creative eye that translates through my previous experience directly into Web Development. Passion for learning and growing with extreme attention to detail which provides dedication to any project.
+          <p className  = "about-me quote-wrapper text-center">I am an aspiring Full Stack Web Developer/Software Engineer with a background in Marketing, Project Management and Graphic Design. I have a creative eye that translates through my previous experience directly into Web Development. I'm passionate about learning and growing, with extreme attention to detail, which provides dedication to any project.
           <br></br>
           <br></br>
           Fun fact: I've recently become an obsessive plant mom</p>

5afec5b1dbfb171306b7ec3151dcc70b5186150a
gaughanln <gaughanln@yahoo.com>
2023-04-25 16:39:04 -0500

    minor edits + styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -66,31 +66,6 @@ function Header({ currentPage, handlePageChange }) {
           </li>
         </ul>
       </nav>
-
-      {/* footer */}
-      {/* <div class="container footer-container">
-        <footer class="d-flex flex-wrap justify-content-center align-items-center  border-top">
-          <a href="https://www.linkedin.com/in/gaughanln/" target="_blank">
-            <img
-              src={linkedin}
-              width="40"
-              height="40"
-              className="icon-nav"
-              alt="linkedin icon"
-            />
-          </a>
-
-          <a href="https://github.com/gaughanln" target="_blank">
-            <img
-              src={github}
-              width="40"
-              height="40"
-              className="icon-nav "
-              alt="github icon"
-            />
-          </a>
-        </footer>
-      </div> */}
     </>
   );
 }

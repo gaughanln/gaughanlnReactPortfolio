f883b5f3ccf7c88b88755385033171cea1d1e7d1
gaughanln <gaughanln@yahoo.com>
2023-05-25 11:03:02 -0500

    update build for gh-pages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ function Contact() {
   return (
     <>
       <div className="container contact-container ">
-        <div className="row h-100">
+        <div className="row align-items-center">
           <div className="col-md-6">
             {/* contact graphic */}
             <img
@@ -16,67 +16,25 @@ function Contact() {
           </div>
 
           {/* contact form */}
-          <div className="col-6">
-            <div className="row align-items-center d-flex justify-content-center ">
-              <div className="mx-auto col-10 col-md-8 col-lg-6">
-                <h3>Want to get in touch?</h3>
-                <h4>Fill out the form below and let's connect!</h4>
+          <div className="col-md-6  text-center">
+            {/* <div className="row align-items-center d-flex justify-content-center "> */}
+             
+              <h2>
+                  Let's connect! 
+                </h2>
+                <p>Shoot me an email and I'll get back to you. </p>
+    
+
+                  <a href ="mailto: gaughanln@yahoo.com" className="btn rounded-pill email">
+                  👋🏼 Email me!                    
+                  </a>
+              
                 <br />
-                <form className="form" id="contact-form">
-                  {/* name */}
-                  <div className="mb-3">
-                    <label for="name" className="form-label">
-                      Your name
-                    </label>
-                    <input
-                      type="name"
-                      className="form-control contact"
-                      required
-                    />
-                  </div>
-
-                  {/* email */}
-                  <div className="mb-3">
-                    <label for="email" className="form-label">
-                      Email address
-                    </label>
-                    <input
-                      type="email"
-                      className="form-control contact"
-                      placeholder="name@example.com"
-                      required
-                    />
-                  </div>
-
-                  {/* message */}
-                  <div className="mb-3">
-                    <label for="message" className="form-label">
-                      I'd love to hear from you!
-                    </label>
-                    <textarea
-                      className="form-control contact"
-                      rows="8"
-                      required
-                    ></textarea>
-                  </div>
-
-                  <button
-                    type="submit"
-                    className="btn rounded-pill"
-                    data-toggle="modal"
-                    data-target="#modalCenter"
-                  >
-                    Submit
-                  </button>
-                </form>
-                <br />
-                <h6>
-                  This form is under construction! in the meantime, you can just
-                  shoot me an email at gaughanln@yahoo.com
-                </h6>
-              </div>
+                
+                
+  
             </div>
-          </div>
+     
         </div>
       </div>
     </>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,10 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import { EffectCoverflow, Pagination } from "swiper";
 
 import playlist from "../assets/PlaylistMania.png";
 import cityBike from "../assets/CityBike1.png";
@@ -6,17 +12,16 @@ import employeeRoster from "../assets/EmployeeRoster.png";
 import portfolio from "../assets/portfolioScreenshot.png";
 import ecommerce from "../assets/backendScreenshot.png";
 import notes from "../assets/NoteTaker.png";
-import brewtique from "../assets/brewtique.png"
+import brewtique from "../assets/brewtique.png";
 
 function Projects() {
-  // Project Data
   const projectData = [
     {
       id: "1",
       title: "Playlist Mania",
       image: playlist,
       description:
-        "Tired of listening to the same chart toppers that play on repeat? Looking for new music? Welcome to Playlist Mania. Here you will be able to search for an artist and explore new songs to add to your favorites and generate your own playlist. The search results will give you samples to listen to so you can decide if you like it before you download it. (Cloud storage is precious these days!) You will need to login and create an account as well.",
+        "With Playlist Mania you will be able to search for an artist and explore new songs to add to your favorites and generate your own playlist. The search results will give you samples to listen to so you can decide if you like it before you download it.",
       github: "https://github.com/gaughanln/PlaylistMania",
       website: "https://playlist-mania-gp2.herokuapp.com/",
     },
@@ -43,7 +48,7 @@ function Projects() {
       title: "Portfolio Reworked",
       image: portfolio,
       description:
-        "This is my second attempt at creating and playing around with my portfolio, this time using materialize + CSS to style. Materialize was used for the CityBike project which I did not get to explore that part as I worked on the Javascript. I wanted to continue learning new programs so I took a stab at this.",
+        "My second attempt at creating and playing around with my portfolio, this time using materialize + CSS to style. ",
       github: "https://github.com/gaughanln/Gaughanln-Portfolio",
       website: "https://gaughanln.github.io/Gaughanln-Portfolio/#project1",
     },
@@ -70,78 +75,149 @@ function Projects() {
       title: "Backend eCommerce",
       image: ecommerce,
       description:
-        "Another employee database type application. You can view all employees, their position within the company, salary, manager, department, and more. If any edits need to be made, they can be done so within the command line. Main concept of this project was to successfully create, update, and delete data in the database when testing API POST, PUT, and DELETE. The project uses Sequelize, Mysql2, and dotenv. The project was not deployed to a live website.",
+        "Main concept of this project was to successfully create, update, and delete data in the database. The project uses Sequelize, Mysql2, and dotenv. If any edits need to be made, they can be done so within the command line.",
       github: "https://github.com/gaughanln/BackEnd_E-Commerce",
       website: null,
     },
   ];
 
-  // Project cards
   return (
     <>
-      <div className="row project-padding">
-        <div className="col-4 d-none d-md-block">
-          <div id="list-example" className="list-group">
-            {projectData.map((project) => (
-              <a
-                className="list-group-item list-group-item-action rounded-pill"
-                href={`#${project.id}`}
-              >
-                {project.title}
-              </a>
-            ))}
-          </div>
-        </div>
+      <div className="carousel">
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          loop={true}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+          breakpoints={{
+            640: {
+                slidesPerView: 2
+            },
+            768: {
+                slidesPerView: 1
+            },
+            1024: {
+                slidesPerView: 2
+            },
+            1560: {
+                slidesPerView: 3
+            },
+        }}
+        >
+          {projectData.map((project) => (
+            <SwiperSlide>
+              <div className="text-center">
+                <img
+                  src={project.image}
+                  className="rounded project-pics"
+                  alt={project.title}
+                />
 
-        <div className="col-8 project-box">
-          <div
-            data-bs-spy="scroll"
-            data-bs-target="#list-example"
-            data-bs-smooth-scroll="true"
-            className="scrollspy-example"
-            tabindex="0"
-          >
-            {projectData.map((project) => (
-              <div id={project.id} className="card text-center">
-                <div className="row">
-                  <div className="col-md-7">
-                    <img
-                      src={project.image}
-                      className="project-images rounded"
-                      alt={project.title}
-                    />
-                  </div>
-                  <div className="col-md-5">
-                    <h3 className="card-title">{project.title}</h3>
-                    <p className="card-text">{project.description}</p>
-                    <a
-                      href={project.github}
-                      target="_blank"
-                      rel="noreferrer"
-                      className="project-links rounded-pill"
-                    >
-                      github
-                    </a>
-
-                    {project.website && (
-                      <a
-                        href={project.website}
-                        target="_blank"
-                        rel="noreferrer"
-                        className="project-links rounded-pill"
-                      >
-                        Website
-                      </a>
-                    )}
-                  </div>
-                </div>
+                <h3 className="text-center project-title">{project.title}</h3>
+                <p className>{project.description}</p>
+                <a
+                  href={project.github}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="project-links rounded-pill"
+                >
+                  Github
+                </a>
+                {project.website && (
+                <a
+                  href={project.website}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="project-links rounded-pill"
+                >
+                  Website
+                </a>
+                  )}
               </div>
-            ))}
-          </div>
-        </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </>
   );
 }
+
+// Project cards
+// return (
+//   <>
+//     <div className="row project-padding">
+//       <div className="col-4 d-none d-md-block">
+//         <div id="list-example" className="list-group">
+//           {projectData.map((project) => (
+//             <a
+//               className="list-group-item list-group-item-action rounded-pill"
+//               href={`#${project.id}`}
+//             >
+//               {project.title}
+//             </a>
+//           ))}
+//         </div>
+//       </div>
+
+//       <div className="col-8 project-box">
+//         <div
+//           data-bs-spy="scroll"
+//           data-bs-target="#list-example"
+//           data-bs-smooth-scroll="true"
+//           className="scrollspy-example"
+//           tabindex="0"
+//         >
+//           {projectData.map((project) => (
+//             <div id={project.id} className="card text-center">
+//               <div className="row">
+//                 <div className="col-md-7">
+//                   <img
+//                     src={project.image}
+//                     className="project-images rounded"
+//                     alt={project.title}
+//                   />
+//                 </div>
+//                 <div className="col-md-5">
+//                   <h3 className="card-title">{project.title}</h3>
+//                   <p className="card-text">{project.description}</p>
+//                   <a
+//                     href={project.github}
+//                     target="_blank"
+//                     rel="noreferrer"
+//                     className="project-links rounded-pill"
+//                   >
+//                     github
+//                   </a>
+
+//                   {project.website && (
+//                     <a
+//                       href={project.website}
+//                       target="_blank"
+//                       rel="noreferrer"
+//                       className="project-links rounded-pill"
+//                     >
+//                       Website
+//                     </a>
+//                   )}
+//                 </div>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   </>
+// );
 
 export default Projects;
